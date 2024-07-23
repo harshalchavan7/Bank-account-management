d7c3f215cc59c34ecaea6b6bbca37ae867e40213
harshalchavan7 <harshalschavan2003@gmail.com>
2024-07-23 18:39:51 +0530

    Added Menu

---
+++ Interface/Menu.java
@@ -0,0 +1,15 @@
+package Interface;
+public class Menu
+{
+    public static void showMenu()
+    {
+        System.out.println("\n1. View balance");
+        System.out.println("2. Deposit money");
+        System.out.println("3. Withdraw money");
+        System.out.println("4. Take loan");
+        System.out.println("5. Pay loan");
+        System.out.println("6. Show loan status");
+        System.out.println("7. Exit");
+        System.out.print("Enter action to perform: ");
+    }
+}
